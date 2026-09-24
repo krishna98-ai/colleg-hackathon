5a6d0b8217cf0027f6da019043d996813457346d
lavish <lavishkamboj430@gmail.com>
2026-09-24 14:41:33 +0530

    sub function add

--- abc.js
+++ abc.js
@@ -1,6 +1,10 @@
-const add = (a,b)=>
-{
-    return a+b
+const add = (a, b) => {
+    return a + b
 }
 
-console.log(add(10,20))
+console.log(add(10, 20))
+
+const sub = () => {
+    return a - b
+}
+console.log(sub(10, 20))
